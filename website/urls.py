6b140d28fca23d50b7ea532f1026af6244bf572b
Chris Eberle <eberle1080@gmail.com>
2013-06-02 20:14:40 -0600

    Fixed media url

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import *
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,4 +13,4 @@ urlpatterns = patterns('',
     (r'^humans\.txt$', TemplateView.as_view(template_name='humans.txt', content_type='text/plain')),
     (r'^favicon\.ico$', RedirectView.as_view(url='/media/img/favicon.ico')),
     (r'^', include('website.tesserae_ng.urls'))
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
