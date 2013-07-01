6f0c696924c73924238ffb4d7c5350ec350b56da
Chris Eberle <eberle1080@gmail.com>
2013-06-30 18:26:44 -0600

    Admin interface works again

--- website/tesserae_ng/models.py
+++ website/tesserae_ng/models.py
@@ -19,7 +19,6 @@ class SourceText(models.Model, DirtyFieldsMixin):
     print_source_link = models.URLField(max_length=4096, null=True, blank=True)
     enabled = models.BooleanField(default=True)
 
-reversion.register(SourceText)
 
 class SourceTextAdmin(reversion.VersionAdmin):
     list_display = ('title', 'author', 'language')
@@ -33,12 +32,12 @@ class SourceTextAdmin(reversion.VersionAdmin):
 
 admin.site.register(SourceText, SourceTextAdmin)
 
+
 class SourceTextVolume(models.Model, DirtyFieldsMixin):
     source = models.ForeignKey(SourceText, on_delete=models.PROTECT)
     volume = models.CharField(max_length=255)
     text = models.TextField(db_index=False)
 
-reversion.register(SourceTextVolume)
 
 class SourceTextVolumeAdmin(reversion.VersionAdmin):
     list_display = ('source', 'volume')
@@ -54,13 +53,13 @@ class SourceTextVolumeAdmin(reversion.VersionAdmin):
 
 admin.site.register(SourceTextVolume, SourceTextVolumeAdmin)
 
+
 class SourceTextSentence(models.Model, DirtyFieldsMixin):
     volume = models.ForeignKey(SourceTextVolume, on_delete=models.PROTECT)
     sentence = models.TextField(db_index=False)
     start_line = models.CharField(max_length=255)
     end_line = models.CharField(max_length=255)
 
-reversion.register(SourceTextSentence)
 
 class SourceTextSentenceAdmin(reversion.VersionAdmin):
     list_display = ('volume', 'sentence',)
